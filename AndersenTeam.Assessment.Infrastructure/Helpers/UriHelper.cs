4cc59dfd9b0a49bd26f1be3b74a00402720012ea
v.holub <holubvasy@gmail.com>
2025-03-19 01:59:29 +0300

    feat(uri-helper): update BaseUri and add new constant
    Updated the BaseUri in the UriHelper class to point to a local development server (https://localhost:44369/) instead of the previous remote server (https://dev-assessment2.andersenlab.com). Added a new constant RequirementsAssessment with the value "api/new/Assessment/assessmentRequirements" to represent an endpoint for assessment requirements.

--- AndersenTeam.Assessment.Infrastructure/Helpers/UriHelper.cs
+++ AndersenTeam.Assessment.Infrastructure/Helpers/UriHelper.cs
@@ -2,9 +2,11 @@
 
 public class UriHelper
 {
-    public const string BaseUri="https://dev-assessment2.andersenlab.com";
+    public const string BaseUri= "https://localhost:44369/";
     
     public const string AssessmentEditUri="api/new/Assessment/edit";
 
     public const string RoleValidationUri = "api/DevTest/GetRoleEmployeeValidation";
+
+    public const string RequirementsAssessment = "api/new/Assessment/assessmentRequirements";
 }
